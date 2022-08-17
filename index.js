b3855ffb0c60a8ae2d2272fdd7ef9445250aafa5
sumitghuraiya <sumitghuraiya@gmail.com>
2022-08-17 23:19:36 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var style=document.documentElement.style;
+
 setInterval(timess,1000)
 function timess(){
+    
     const t=new Date();
-    var se=t.getSeconds()*6+270;
     var min=t.getMinutes()*6+270;
     var hous=t.getHours()*30+270;
-    console.log(t);
+    var se=t.getSeconds()*6+270;
+    style.setProperty("--second",se+"deg");
     style.setProperty("--hour",hous+"deg");
     style.setProperty("--minute",min+"deg");
-    style.setProperty("--second",se+"deg");
 }
